9881447462d563128e58d7fed67a15578f1c8c12
zhexxxian <zhangzhexian@outlook.com>
2017-01-10 23:07:51 +0800

    Game over buttons implementation

---
+++ Assets/Scripts/GameOverButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Assets.Scripts;
+
+public class GameOverButton : MonoBehaviour {
+
+	public void PlayAgain_button_click() {
+		MazeDatabase.GenerateMaze(GlobalVariable.CurrentLevel);
+		SceneManager.LoadScene("game-scene");
+	}
+
+	public void MainMenu_button_click() {
+		SceneManager.LoadScene("menu-scene");
+	}
+
+
+}
